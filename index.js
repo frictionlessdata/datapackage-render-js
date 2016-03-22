5367b364888820c7109e37c0f34bcce2fbec27b4
Rufus Pollock <rufus.pollock@okfn.org>
2016-03-22 18:36:58 +0100

    [all][l]: working vega + resource renderer.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var nunjucks = require('nunjucks')
   , parse = require('csv-parse')
   , transform = require('stream-transform')
   , fs = require('fs')
+  , vl = require("vega-lite")
+  , vg = require("vega")
   ;
 
 exports.html = function(path, callback) {
@@ -36,9 +38,13 @@ exports.toolsDataProxy = function(req, res) {
 }
 
 // given a resource, give me a stream containing json objects representing data in the stream
+
+// if a callback is provided then rather than return the stream return an array containing the rows of the data (as parsed)
+
 // for tabular data this is a stream of the row objects
 // TODO: for geo probably something a bit different
-exports.resourceStream = function(resource) {
+// TODO: cast data using the schema
+exports.resourceStream = function(resource, callback) {
   var parser = parse({delimiter: ','});
 
   if (resource.format && resource.format != 'csv') {
@@ -52,7 +58,27 @@ exports.resourceStream = function(resource) {
     return data;
   });
   var stream = exports.rawResourceStream(resource);
-  return stream.pipe(parser).pipe(transformer);
+  var outstream = stream.pipe(parser).pipe(transformer);
+  if (!callback) {
+    return outstream;
+  } else {
+    exports.objectStreamToArray(outstream, callback);
+  }
+}
+
+exports.objectStreamToArray = function(stream, callback) {
+  var output = [];
+  stream.on('readable', function() {
+    while(row = stream.read()) {
+      output.push(row);
+    }
+  });
+  stream.on('error', function(error) {
+    callback(error);
+  });
+  stream.on('finish', function() {
+    callback(null, output);
+  });
 }
 
 // get me a resource from a datapackage identified by index or name
@@ -61,11 +87,11 @@ exports.resourceByIdentifier = function(datapackage, resourceIdentifier) {
     , resource = null
     ;
 
-  if (resourceName.match('^\d+$')) {
-    resourceIndex = parseInt(resourceName)
+  if (resourceIdentifier.match('^\d+$')) {
+    resourceIndex = parseInt(resourceIdentifier)
   } else {
     datapackage.resources.forEach(function(res, idx) {
-      if (res.name === resourceName) {
+      if (res.name === resourceIdentifier) {
         resourceIndex = idx;
       }
     });
@@ -86,9 +112,60 @@ exports.rawResourceStream = function(resource) {
   } else if (resource.path) {
     return fs.createReadStream(resource.path);
   } else if (resource.data) {
-    // TODO
+    // TODO: what happens if it is already JSON objects ...?
+    return streamFromString(resource.data);
   } else {
     return null
   }
+}
+
+function streamFromString(string) {
+  var s = new stream.Readable();
+  s._read = function noop() {}; // redundant? see update below
+  s.push(string);
+  s.push(null);
+}
+
+// get stream representing vega data
+exports.getVegaDataFromViewDataSpec = function(datapackage, viewId, callback) {
+  var viewDataSpec = datapackage.views[viewId].data[0];
+  // TODO: transforms etc ...
+  var resource = exports.resourceByIdentifier(datapackage, viewDataSpec.resource);
+  exports.resourceStream(resource, function(error, data) {
+    var out = [
+      {
+        "name": viewDataSpec.resource,
+        "values": data
+      }
+    ]
+    callback(error, out);
+  });
+}
+ 
+
+// Render a Data Package view to a Vega view component
+
+// 1. get the data for this view and convert to vega structure (note: vega structure not 
+// 2. 
+exports.renderView = function(datapackage, viewId, callback) {
+  var view = datapackage.views[viewId]
+    , vgSpec = vl.compile(view.spec).spec;
+    ;
+  exports.getVegaDataFromViewDataSpec(datapackage, viewId, function(error, data) {
+    vg.parse.spec(vgSpec, {data: data}, function(error, chart) {
+      var vegaView = chart();
+      callback(error, vegaView);
+    });
+  });
+}
+
+function writeToPng(vegaView, file, callback) {
+  vegaView.canvasAsync(function(canvas) {
+    // writePNG(canvas, outputFile);
+    var file = null;
+    var out = file ? fs.createWriteStream(file) : process.stdout;
+    var stream = canvas.createPNGStream();
+    stream.pipe(out);
+  });
 }
 
--- test/all.js
+++ test/all.js
@@ -2,6 +2,64 @@ var assert = require('assert')
   , spec = require('../index.js')
   ;
 
+var dp1 = {
+  "name": "abc",
+  "resources": [
+    {
+      "name": "random",
+      "format": "csv",
+      "path": "test/data/dp1/data.csv"
+    }
+  ],
+  "views": [
+    {
+      "data": [
+        {
+          "resource": "random"
+        }
+      ],
+      "spec": {
+        "mark": "bar",
+        "encoding": {
+          "x": {"field": "a", "type": "ordinal"},
+          "y": {"field": "b", "type": "quantitative"}
+        }
+      }
+    }
+  ]
+};
+
+dp2 = {
+  "name": "abc",
+  "resources": [
+    {
+      "name": "random",
+      "format": "json",
+      "data": [
+        {"a": "A","b": 28}, {"a": "B","b": 55}, {"a": "C","b": 43},
+        {"a": "D","b": 91}, {"a": "E","b": 81}, {"a": "F","b": 53},
+        {"a": "G","b": 19}, {"a": "H","b": 87}, {"a": "I","b": 52}
+      ]
+    }
+  ],
+  "views": [
+    {
+      "data": [
+        {
+          "resource": "random"
+        }
+      ],
+      "spec": {
+        "mark": "bar",
+        "encoding": {
+          "x": {"field": "a", "type": "ordinal"},
+          "y": {"field": "b", "type": "quantitative"}
+        }
+      }
+    }
+  ]
+};
+
 describe('html', function() {
   it('html renders ok', function(done) {
     spec.html('test/data/dp1', function(error, html) {
@@ -13,29 +71,51 @@ describe('html', function() {
 });
 
 describe('resourceStream', function() {
+  var resource = {
+    "path": "test/data/dp1/data.csv"
+  }
   it('works ok', function(done) {
-    var resource = {
-      "path": "test/data/dp1/data.csv"
-    }
-    datastream = spec.resourceStream(resource);
-    var output = [];
-    datastream.on('readable', function() {
-      while(row = datastream.read()) {
-        output.push(row);
-      }
-    });
-    datastream.on('finish', function() {
+    spec.resourceStream(resource, function(error, output) {
+      assert(!error);
       assert.equal(output.length, 3);
       assert.equal(output[0].size, "100");
       done();
     });
+  });
+  it('works with stream', function(done) {
+    var stream = spec.resourceStream(resource);
+    spec.objectStreamToArray(stream, function(error, output) { 
+      assert(!error);
+      assert.equal(output.length, 3);
+      assert.equal(output[0].size, "100");
+      done();
+    });
+  });
+});
 
-//    spec.resourceStream(resource, function(error, datastream) {
-//      assert(!error);
-//      assert.equal(datastream.length, 3);
-//      assert.equal(datastream[0].size, "100");
-//      done();
-//    });
+describe('misc', function() {
+  it('getVegaDataFromViewDataSpec works ok', function(done) {
+    spec.getVegaDataFromViewDataSpec(dp1, 0, function(err, data) {
+      assert.equal(data[0].values[0].name, 'gb');
+      done();
+    }); 
+  });
+});
+
+describe('renderView', function() {
+  it('works ok', function(done) {
+    spec.renderView(dp1, 0, function(error, vegaView) {
+      vegaView.renderer('canvas').update();
+      var stream = vegaView.canvas().createPNGStream();
+      var output = [];
+      stream.on('data', function(chunk) {
+        output.push(chunk);
+      });
+      stream.on('end', function() {
+        assert.equal(output[0][0], 137);
+        done();
+      });
+    });
   });
 });
 
